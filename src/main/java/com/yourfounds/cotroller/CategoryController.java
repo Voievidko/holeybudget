6748e7629326b05fbc8c364a9e51a9d776c095a6
Roman Voievidko <voievidko@gmail.com>
2019-06-15 14:41:18 +0300

    implement controllers for Categories and Expenses

---
+++ src/main/java/com/yourfounds/cotroller/CategoryController.java
@@ -0,0 +1,96 @@
+package com.yourfounds.cotroller;
+
+import com.yourfounds.entity.Category;
+import com.yourfounds.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("all")
+    public String listCategories (Model model){
+        // get categories from DAO
+        List<Category> categories = categoryService.getCategories();
+
+        // add customers to the model
+        model.addAttribute("categories", categories);
+        return "category/all";
+    }
+
+    @RequestMapping("add")
+    public String addCategory(Model model){
+        Category category = new Category();
+
+        model.addAttribute("category", category);
+        return "category/add";
+    }
+
+    @RequestMapping("addProcess")
+    public String processAddCategoryForm(@ModelAttribute("category") Category category, Model model){
+        if (categoryService.isCategoryNameExist(category)){
+            model.addAttribute("category", category);
+            return "category/nameexist";
+        }
+        categoryService.addCategory(category);
+        return "success";
+    }
+
+    @RequestMapping("delete")
+    public String deleteCategory(@ModelAttribute("categoryId") int categoryId, Model model){
+        //check if there is relationship on category
+        if (categoryService.isCategoryHaveRelations(categoryId)){
+            //inform User that category can't be deleted
+            //and propose to create new category
+            //or transfer expenses to exist category
+            Category categoryToDelete = categoryService.getCategory(categoryId);
+            List<Category> categories = categoryService.getCategories();
+            categories.remove(categoryToDelete);
+
+            Category category = new Category();
+            model.addAttribute("categoryToDelete", categoryToDelete);
+            model.addAttribute("categories", categories);
+            model.addAttribute("category", category);
+            return "category/cantdelete";
+        } else {
+            categoryService.deleteCategoryById(categoryId);
+            return "success";
+        }
+    }
+
+    @GetMapping("update")
+    public String updateCategory(@ModelAttribute("categoryId") int categoryId, Model model){
+        Category category = categoryService.getCategory(categoryId);
+        model.addAttribute("category", category);
+        return "category/update";
+    }
+
+    @RequestMapping("updateprocess")
+    public String updateProcess(@ModelAttribute("category") Category category){
+        categoryService.updateCategory(category);
+        return "redirect:all";
+    }
+
+    @RequestMapping("transferToExistCategory")
+    public String transferToOtherCategoryAndDelete(@ModelAttribute("categoryId") int toCategoryId, @ModelAttribute("categoryToDelete") int fromCategoryId, Model model){
+        categoryService.replaceCategoryInAllExpenses(fromCategoryId, toCategoryId);
+        return "success";
+    }
+
+    @RequestMapping("transferToNewCategory")
+    public String transferToNewCategoryAndDelete(@ModelAttribute("newCategory") Category category, @ModelAttribute("categoryToDelete") int fromCategoryId, Model model){
+        categoryService.addCategory(category);
+        categoryService.replaceCategoryInAllExpenses(fromCategoryId, category.getCategoryId());
+        return "success";
+    }
+}
---
+++ src/main/java/com/yourfounds/cotroller/ExpenseController.java
@@ -0,0 +1,99 @@
+package com.yourfounds.cotroller;
+
+import com.yourfounds.entity.Category;
+import com.yourfounds.entity.Expense;
+import com.yourfounds.entity.User;
+import com.yourfounds.service.CategoryService;
+import com.yourfounds.service.ExpenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+@Controller
+@RequestMapping("expense")
+public class ExpenseController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private ExpenseService expenseService;
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        dateFormat.setLenient(false);
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
+    }
+
+    @RequestMapping("add")
+    public String addForm(Model model){
+        Expense expense = new Expense();
+
+        List<Category> categories = categoryService.getCategories();
+        expense.setDate(new Date());
+
+        model.addAttribute("categories", categories);
+        model.addAttribute("expense", expense);
+
+        return "expense/add";
+    }
+
+    //Annotation Valid from javax Validation will check data
+    //Validation annotation should be in POJO class like @NotNull, @Min
+    @RequestMapping("addProcess")
+    public String processForm(@Valid @ModelAttribute("expense") Expense expense, BindingResult bindingResult,
+                              @ModelAttribute("tempCategory") Category category, Model model){
+        if(bindingResult.hasErrors()){
+            List<Category> categories = categoryService.getCategories();
+            expense.setDate(new Date());
+            model.addAttribute("categories", categories);
+            return "expense/add";
+        }
+        //todo: User is hardcoded. Replace it when implement needed logic.
+        User user = new User();
+        user.setUserId(1);
+        expense.setCategory(categoryService.getCategory(category.getCategoryId()));
+        expense.setTime(new Date());
+        expense.setUser(user);
+        expenseService.addExpense(expense);
+        return "redirect:add";
+    }
+
+    @RequestMapping("delete")
+    public String deleteExpense(@ModelAttribute("expenseId") int expenseId){
+        expenseService.deleteExpenseById(expenseId);
+        return "success";
+    }
+
+    @RequestMapping("all")
+    public String showAll(Model model){
+        List<Expense> expenses = expenseService.getAllExpenses();
+        model.addAttribute("expenseName", "Expenses during all time");
+        model.addAttribute("expenses", expenses);
+        model.addAttribute("totalSum", expenses.stream().map(a -> a.getSum()).reduce(0d, (a, b) -> a + b));
+        return "expense/all";
+    }
+
+    @RequestMapping("currentmonth")
+    public String showCurrentMonth(Model model){
+        List<Expense> expenses = expenseService.getExpensesDuringCurrentMonth();
+        model.addAttribute("expenseName", "Expenses during " + expenseService.getCurrentMonthName());
+        model.addAttribute("expenses", expenses);
+        model.addAttribute("totalSum", expenses.stream().map(a -> a.getSum()).reduce(0d, (a, b) -> a + b));
+        return "expense/all";
+    }
+
+}

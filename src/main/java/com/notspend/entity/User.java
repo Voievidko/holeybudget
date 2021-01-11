9734b5e3fb680615fd7925c55dbe8dbf47fc7834
iv.gonchar <gonchar.ivan@gmail.com>
2021-01-11 03:53:31 +0200

    Add lombok for User

--- src/main/java/com/notspend/entity/User.java
+++ src/main/java/com/notspend/entity/User.java
@@ -1,6 +1,9 @@
 package com.notspend.entity;
 
 import com.notspend.validation.ValidEmail;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
@@ -9,6 +12,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Data
+@EqualsAndHashCode(of = "username")
+@ToString(of = {"username", "email", "name", "surname", "enabled", })
 @Table(name = "user")
 public class User {
 
@@ -40,94 +46,21 @@ public class User {
     @OneToMany (mappedBy = "user",
                 cascade = {CascadeType.PERSIST, CascadeType.MERGE,
                            CascadeType.DETACH, CascadeType.REFRESH})
-    private List<Expense> expenses;
+    private List<Expense> expenses = new ArrayList<>();
 
     @OneToMany (mappedBy = "user", fetch = FetchType.EAGER,
                 cascade = {CascadeType.PERSIST, CascadeType.MERGE,
                            CascadeType.DETACH, CascadeType.REFRESH})
-    private List<Account> accounts;
-
-    public User() {
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public boolean getEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    public List<Expense> getExpenses() {
-        return expenses;
-    }
-
-    public void setExpenses(List<Expense> expenses) {
-        this.expenses = expenses;
-    }
+    private List<Account> accounts = new ArrayList<>();
 
     //for bi-directional relationship
     public void setExpence(Expense expence) {
-        if (expenses == null){
-            expenses = new ArrayList<>();
-        }
         expenses.add(expence);
         expence.setUser(this);
     }
 
-    public List<Account> getAccounts() {
-        return accounts;
-    }
-
-    public void setAccounts(List<Account> accounts) {
-        this.accounts = accounts;
-    }
-
     //for bi-directional relationship
     public void setAccount(Account account){
-        if(accounts == null){
-            accounts = new ArrayList<>();
-        }
         accounts.add(account);
         account.setUser(this);
     }

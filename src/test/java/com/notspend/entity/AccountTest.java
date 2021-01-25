a1f9b71f9637f40ca606e315b6a72d7805e5c750
iv.gonchar <gonchar.ivan@gmail.com>
2021-01-26 00:02:22 +0200

    Add unit test for Account

---
+++ src/test/java/com/notspend/entity/AccountTest.java
@@ -0,0 +1,34 @@
+package com.notspend.entity;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AccountTest {
+
+    private Account account = new Account();
+
+    @BeforeEach
+    void setUp() {
+        account.setSummary(120.53);
+    }
+
+    @Test
+    void substract() {
+        account.substract(20.53);
+        assertEquals(Double.valueOf(100.00), account.getSummary());
+    }
+
+    @Test
+    void plus() {
+        account.plus(29.47);
+        assertEquals(Double.valueOf(150.00), account.getSummary());
+    }
+
+    @Test
+    void minus() {
+        account.minus(20.53);
+        assertEquals(Double.valueOf(100.00), account.getSummary());
+    }
+}

4f4c061ae7564538e3a944e98b2969c3c70acf6f
Roman Voievidko <roman.voievidko@nordstrom.com>
2022-04-11 12:33:03 +0200

    use unknown mcc code as default

--- src/main/java/com/holeybudget/dao/impl/MccDaoImpl.java
+++ src/main/java/com/holeybudget/dao/impl/MccDaoImpl.java
@@ -2,13 +2,17 @@ package com.holeybudget.dao.impl;
 
 import com.holeybudget.dao.MccDao;
 import com.holeybudget.entity.Mcc;
+import lombok.extern.apachecommons.CommonsLog;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@CommonsLog
 public class MccDaoImpl implements MccDao {
+
+    private static final Integer UNKNOWN_MCC_CODE = 9999;
 
     @Autowired
     private SessionFactory sessionFactory;
@@ -16,6 +20,12 @@ public class MccDaoImpl implements MccDao {
     @Override
     public String getCategoryByMccCode(Integer code) {
         Session session = sessionFactory.getCurrentSession();
-        return session.get(Mcc.class, code).getCategoryName();
+        Mcc mcc = session.get(Mcc.class, code);
+        if (mcc == null) {
+            log.warn("Table with MCC codes doesn't have code: '" + code +
+                "'. Use unknown code '" + UNKNOWN_MCC_CODE + "' instead.");
+            mcc = session.get(Mcc.class, UNKNOWN_MCC_CODE);
+        }
+        return mcc.getCategoryName();
     }
 }
